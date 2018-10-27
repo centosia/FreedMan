1dedda354111124431987616b905a46a76582489
centosia <44514898+centosia@users.noreply.github.com>
2018-10-27 07:26:55 +0530

    Create univs.js

---
+++ univs.js
@@ -0,0 +1,3 @@
+var univs = function(university_name) {
+        console.log(university_name+"is only out there to make money. Its not there to select students with great minds, just great GPA");
+}
